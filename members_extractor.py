5a6899ee30783b46a7c43c605bd0b50beaaec3e9
kkosyan <lizhenina.k@gmail.com>
2023-09-21 17:06:44 +0300

    modify members extractor

--- members_extractor.py
+++ members_extractor.py
@@ -1,4 +1,5 @@
 import gitlab
+from gitlab.v4.objects.projects import Project
 
 
 class MembersExtractor:
@@ -9,7 +10,18 @@ class MembersExtractor:
     def _auth(self):
         return gitlab.Gitlab(url=self._url, private_token=self._token)
 
+    @staticmethod
+    def _get_members_names(project: Project):
+        members = project.members_all.list(get_all=True)
+        return [
+            member.username
+            for member in members
+        ]
+
     def get_members(self):
         gl = self._auth()
-        project = gl.projects.list(get_all=True)
-        return project.members_all.list(get_all=True)
+        projects = gl.projects.list(get_all=True)
+        return [
+            (project.id, project.path_with_namespace, self._get_members_names(project=project))
+            for project in projects
+        ]
